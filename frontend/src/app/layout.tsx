0e474104f7ea2c5927136abedadce5f45b47d6d7
pzwz <na.univi@gmail.com>
2025-11-05 16:37:44 +0300

    اعادة تحميل تايلويند
    + اضافة global.css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,11 +7,7 @@ export const metadata: Metadata = {
   description: "Smart Lands Frontend",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
