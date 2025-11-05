2f3781f225a200122b5ae874df43a3df70600caa
pzwz <na.univi@gmail.com>
2025-11-05 16:21:31 +0300

    اضافة لاياوت اساسي

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,20 @@
+// src/app/layout.tsx
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Smart Lands",
+  description: "Smart Lands Frontend",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
