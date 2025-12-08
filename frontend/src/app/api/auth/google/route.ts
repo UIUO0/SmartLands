02b2f0026067b3759236ce845a465f57ed8a9863
pzwz <na.univi@gmail.com>
2025-12-08 23:46:37 +0300

    Update route.ts

--- frontend/src/app/api/auth/google/route.ts
+++ frontend/src/app/api/auth/google/route.ts
@@ -6,7 +6,9 @@ const BACKEND_URL = "https://smartlands-production.up.railway.app";
 export async function POST(req: Request) {
   try {
     const { id_token } = await req.json();
+    
 
+    
     // إرسال التوكن للباك-إند
     const res = await fetch(`${BACKEND_URL}/auth/google`, {
       method: "POST",
