d86fa37d02f42dbca1be170349ade056fd5180a7
pzwz <na.univi@gmail.com>
2025-12-08 22:45:47 +0300

    added google sign up

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
-// src/app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
+import { GoogleAuthProvider } from  "@/components/GoogleProvider"; // 👈 استدعاء الغلاف الجديد
 
 export const metadata: Metadata = {
   title: "Smart Lands",
@@ -10,7 +10,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* قمنا بإحاطة الأطفال بالبروفايدر 
+            ليتمكن أي زر تسجيل دخول في الموقع من الوصول لجوجل 
+        */}
+        <GoogleAuthProvider>
+           {children}
+        </GoogleAuthProvider>
+      </body>
     </html>
   );
 }
