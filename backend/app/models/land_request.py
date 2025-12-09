48c5f63d8d0701588288f678321b920f283ef29b
UIUO0 <gamilgamil535@gmail.com>
2025-12-09 03:13:41 +0300

    Update land_request.py

--- backend/app/models/land_request.py
+++ backend/app/models/land_request.py
@@ -24,7 +24,7 @@ class LandRequest(Base):
     )
 
     land_id: Mapped[int] = mapped_column(
-        BigInteger,
+        Integer,
         ForeignKey("lands.land_id", ondelete="CASCADE"),
         nullable=False,
     )
